50adda580ab173d8f12bf4e33c00bc12ad8cc554
shu Lin <bbqlinshu@gmail.com>
2022-04-03 00:14:03 -0400

    commit tree d059647ebc465df9d61814365c3f9a06c0cfc34d

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,15 +5,9 @@ class Post < ApplicationRecord
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
     validate :clickbait_y?
 
-    CLICKBAIT = [
-        /Won't Believe/i,
-        /Secret/i,
-        /Top \d/i,
-        /Guess/i
-    ]
     def clickbait_y?
-        if CLICKBAIT.none? { |word| word.match title }
-            errors.add(:title, "must contain words below - Won't Believe, Secret, Top [number], Guess")
+        unless ((/Won't Believe/i.match title) || (/Secret/i.match title) || (/Top \d/i.match title) || (/Guess/i.match title)) 
+            errors.add(:title, "must be clickbait_y")
         end
     end
 
