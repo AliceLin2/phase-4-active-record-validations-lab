0508eb39df59cf22b142aa3377d15a1381156437
shu Lin <bbqlinshu@gmail.com>
2022-04-02 23:38:24 -0400

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_y?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def clickbait_y?
+        if CLICKBAIT.none? { |word| word.match title }
+            errors.add(:title, "must contain words below - Won't Believe, Secret, Top [number], Guess")
+        end
+    end
+
 end
